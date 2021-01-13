dcc60b5e4155eb37655420347caaaa29e1a0f7e8
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2021-01-13 23:53:39 +0530

    Coding Challenge Done

--- src/test/containerTest/App.test.js
+++ src/test/containerTest/App.test.js
@@ -7,8 +7,7 @@
 //   const linkElement = getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
-// npm i @testing-library/jest-dom@4.2.4 @testing-library/react@9.5.0 @testing-library/user-event@7.2.1
-// npm i @testing-library/jest-dom@latest @testing-library/react@latest @testing-library/user-event@latest
+
 import Enzyme, { shallow } from 'enzyme';
 import App from '../../container/App';
 import Adapter from 'enzyme-adapter-react-16';
