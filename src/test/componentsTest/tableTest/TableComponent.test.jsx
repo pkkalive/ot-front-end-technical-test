6ce0d9bddf9c6ee32dd9c091326e25891642d0f2
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2021-01-13 23:39:54 +0530

    Coding Challenge done

--- src/test/componentsTest/tableTest/TableComponent.test.jsx
+++ src/test/componentsTest/tableTest/TableComponent.test.jsx
@@ -1,20 +1,34 @@
+import React from 'react';
+import thunk from 'redux-thunk';
 import * as Redux from 'react-redux';
-import { createStore } from 'redux';
 import Enzyme, { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
 import Adapter from 'enzyme-adapter-react-16';
+import { render } from '@testing-library/react';
 import TableComponent from '../../../components/table/TableComponent';
-import RootReducer from '../../../redux/reducers/RootReducer';
-
-let store = createStore(RootReducer);
+import { expectedMockData } from '../../reduxTest/actionsTest/TableActions.test';
 
 Enzyme.configure({adapter: new Adapter()});
-
-const spy = jest.spyOn(Redux, 'useSelector')
-spy.mockReturnValue({ username:'test' })
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
 
 describe('Table Component', () => {
+  let useEffect;
+  let wrapper;
+  const store = mockStore(expectedMockData);
+  let mockUseEffect = () => {
+    useEffect.mockImplementationOnce(f => Promise.resolve(expectedMockData));
+  };
+  beforeEach(() => {
+    useEffect = jest.spyOn(React, "useEffect");
+    mockUseEffect();
+    wrapper = shallow(<Redux.Provider store={store}><TableComponent /></Redux.Provider>);
+  });
   test("Table Component Test Cases", () => {
     const component = shallow(<Redux.Provider store={store}><TableComponent /></Redux.Provider>)
     expect(component.exists()).toBe(true);
+    expect(wrapper.exists()).toBe(true);
+    const {debug} = render(<Redux.Provider store={store}><TableComponent /></Redux.Provider>);
+    debug();
   })
 })
